5f76912eea6814a8af61518e8f4f7519d01fcbcf
AyaMohammedd <raya80224@gmail.com>
2025-06-02 18:26:46 +0300

    deploy_hand_gesture_for_astary_game

--- app/main.py
+++ app/main.py
@@ -20,10 +20,10 @@ Instrumentator().instrument(app).expose(app)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:5500"], 
+    allow_origins=["*"], 
     allow_credentials=True,
-    allow_methods=["*"],  
-    allow_headers=["*"],  
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 # ---------- GLOBAL START TIME ----------
